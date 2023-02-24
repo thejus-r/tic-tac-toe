11362ca71437bcdf662e8ed85a41d92126c272ce
Thejus Rajendran <thejusr1999@gmail.com>
2023-02-25 01:33:40 +0530

    trying out portals

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import Button from "./components/base/Button";
+import { useState } from "react";
 import Board from "./components/Board";
 import Modal from "./components/Modal";
-import Modal2 from "./components/Modal2";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="w-full h-screen bg-stone-900 text-stone-100 flex flex-col justify-center items-center">
       <h1 className="text-4xl font-bold mb-12">Tic Tac Toe</h1>
       <Board />
-      <Modal />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      <button onClick={() => setIsOpen(true)} className="p-4 bg-green-800">
+        Click to open modal
+      </button>
     </div>
   );
 }
-
 export default App;
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,24 +2,29 @@ import { ReactNode } from "react";
 import { createPortal } from "react-dom";
 import { XMarkIcon } from "@heroicons/react/20/solid";
 
-export default function ModalContainer() {
-  return createPortal(
-    <Backdrop>
-      <Modal />
-    </Backdrop>,
-    document.getElementById("portal")!
-  );
+export default function ModalContainer(props) {
+  return props.isOpen
+    ? createPortal(
+        <Backdrop>
+          <Modal setIsOpen={props.setIsOpen} />
+        </Backdrop>,
+        document.getElementById("portal")!
+      )
+    : null;
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="bg-stone-800 border-2 border-stone-700 rounded-lg w-1/4 gap-4 shadow-black shadow-2xl  flex flex-col justify-between text-white min-h-40 p-4">
       <div className="flex flex-col gap-2">
         <div className="flex justify-between w-full">
           <h4 className="text-lg font-medium">Alert</h4>
-          <div className="w-6 h-6 rounded-full flex justify-center items-center font-bold bg-stone-700">
+          <button
+            onClick={() => props.setIsOpen(false)}
+            className="w-6 h-6 rounded-full flex justify-center items-center font-bold bg-stone-700"
+          >
             <XMarkIcon className="h-4 w-4" />
-          </div>
+          </button>
         </div>
         <p className="text-stone-300">
           Resetting the board, will cause to loose the game progress
@@ -40,7 +45,7 @@ function Modal() {
 
 function Backdrop({ children }: { children: ReactNode }) {
   return (
-    <div className=" absolute top-0 left-0 h-screen w-full bg-black/50 flex justify-center items-center">
+    <div className=" absolute top-0 left-0 h-screen w-full backdrop-blur-md bg-black/50 flex justify-center items-center">
       {children}
     </div>
   );

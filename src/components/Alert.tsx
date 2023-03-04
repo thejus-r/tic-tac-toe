7c9d6cf326d61ad00f8b1045a72ecdaff1594b3b
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-04 12:19:01 +0530

    now shows whos turn is next

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,20 +1,26 @@
-import { ReactNode } from "react";
 import { createPortal } from "react-dom";
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import useGameStore from "../gameStore";
 
-function Alert({context, closeAlert}: {context:string, closeAlert:any}) {
-    return createPortal(<div className="absolute flex flex-col justify-center items-center h-screen w-screen top-0 left-0 bg-black/75 backdrop-blur-md text-white">
-        <div className="bg-stone-900 flex w-1/2 max-w-lg flex-col gap-2 p-4 rounded-lg border-2 border-stone-800">
-<div className="flex flex-col justify-center items-center">
-<div>X</div>
-<p className="text-2xl font-bold uppercase">{context}</p>
-</div>
-            <button onClick={closeAlert} className="py-2 font-medium text-md rounded-lg border-2 border-red-900 bg-red-900/50">Restart</button>
+function Alert({ context }: { context: string }) {
+  const resetGame = useGameStore((state) => state.resetGame);
+  return createPortal(
+    <div className="absolute flex flex-col justify-center items-center h-screen w-screen top-0 left-0 bg-black/75 backdrop-blur-md text-white">
+      <div className="bg-stone-900 flex w-1/2 max-w-lg flex-col gap-2 p-4 rounded-lg border-2 border-stone-800">
+        <div className="flex flex-col justify-center items-center">
+          <div>X</div>
+          <p className="text-2xl font-bold uppercase">{context}</p>
         </div>
+        <button
+          onClick={() => resetGame()}
+          className="py-2 font-medium text-md rounded-lg border-2 border-red-900 bg-red-900/50"
+        >
+          Restart
+        </button>
+      </div>
     </div>,
     document.body
-    )
+  );
 }
 
 export default Alert;
-

2adc7e5731b748a0f917d6a778c836a6f97ebe98
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-05 23:06:38 +0530

    fixed position on game status

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="w-full h-screen bg-stone-900 text-stone-100 flex flex-col justify-center items-center">
       <h1 className="text-4xl font-bold mb-12">Tic Tac Toe</h1>
-      <Board />
       <Updater />
+      <Board />
     </div>
   );
 }
